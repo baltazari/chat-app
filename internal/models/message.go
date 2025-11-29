03b077ed9c6f98b83f9b61c9f6fb83c653ec39af
mishka mrelashvili <mishka.mrelashvili@gmail.com>
2025-11-29 19:22:40 +0400

    add messseg model

---
+++ internal/models/message.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Message struct {
+	ID        uint   `gorm:"primaryKay"`
+	UserID    uint   `gorm:"index;not null"`
+	RoomID    uint   `gorm:"index;not null"`
+	Content   string `gorm:"type:not null"`
+	CreatedAt time.Time
+
+	User User `gorm:"constraint:OnDelete:CASCADE;"`
+	Room Room `gorm:"constraint:OnDelete:CASCADE;"`
+}
--- internal/models/room.go
+++ internal/models/room.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Room struct {
-	ID       uint   `gorm:"primaryKey"`
-	Name     string `gorm:"uniqueIndex;size:150;not null"`
-	CreateAt time.Time
-	UpdateAt time.Time
-	Messages []Message
+	ID        uint   `gorm:"primaryKey"`
+	Name      string `gorm:"uniqueIndex;size:150;not null"`
+	CreatedAt time.Time
+	Updatedt  time.Time
+	Messages  []Message
 }
