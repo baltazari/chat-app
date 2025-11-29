b4f4dac46be87e90205827a9c46866a471a44c1c
mishka mrelashvili <mishka.mrelashvili@gmail.com>
2025-11-29 17:53:55 +0400

    add user model

---
+++ internal/models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        uint   `gorm:"primaryKey"`
+	Username  string `gorm:"uniqueIndex; size: 50; not null"`
+	Password  string `gorm:"size:255; not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+	Messages []Message
+}
